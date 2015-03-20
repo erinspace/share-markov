838b5fd4b3e154958788a3cc4ffcaed58fce2f27
erinspace <erin.braswell@gmail.com>
2015-03-20 09:31:28 -0400

    Add settings and gitignore

---
+++ settings/__init__.py
@@ -0,0 +1,11 @@
+'''Consolidates settings from defaults.py and local.py.
+'''
+
+
+from .defaults import *
+
+try:
+    from .local import *
+except ImportError as error:
+    raise ImportError("No local.py settings file found. Did you remember to "
+                      "copy local-dist.py to local.py?")
---
+++ settings/local-dist.py
@@ -0,0 +1,5 @@
+CONSUMER_KEY = 'your_twitter_consumer_key'
+CONSUMER_SECRET = 'your_twitter_conssumer_secret'
+
+ACCESS_KEY = 'your_twitter_access_key'
+ACCESS_SECRET = 'your_twitter_access_secret'

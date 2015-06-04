c30b2b69c8d0aed2a8126437948c0bb1ea9296ac
erinspace <erin.braswell@gmail.com>
2015-06-04 00:55:40 -0400

    Fix twitter error

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def get_tweet(q, pages, cher=False):
 
     while get_character_count(tweet) > 140:
         tweet = generate_line(
-            description_chain, twitter=True) + ' #MarkovScience'
+            description_chain, cher, twitter=True) + ' #MarkovScience'
 
     print(tweet)
     return tweet

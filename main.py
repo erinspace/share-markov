97bbdefbdb947ee2ea79a758de26d8f5fd464cee
erinspace <erin.braswell@gmail.com>
2015-06-05 18:17:48 -0400

    Help text updates

--- main.py
+++ main.py
@@ -135,9 +135,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description="A command line interface for generating random SHARE tweets...")
 
     parser.add_argument('-q', '--query', dest='query', type=str, help='Query to get the initial SHARE text')
-    parser.add_argument('-p', '--pages', dest='pages', type=int, help=' ')
-    parser.add_argument('-c', '--cher', dest='cher', help='The number of results to return per aggretation', action='store_true')
-    parser.add_argument('-t', '--tweet', dest='tweet', help='The version of the OSF SHARE API to hit', action='store_true')
+    parser.add_argument('-p', '--pages', dest='pages', type=int, help='Number of pages to gather for markov generation')
+    parser.add_argument('-c', '--cher', dest='cher', help='Generate Cher research???', action='store_true')
+    parser.add_argument('-t', '--tweet', dest='tweet', help='To tweet or not to tweet.', action='store_true')
 
     return parser.parse_args()
 
